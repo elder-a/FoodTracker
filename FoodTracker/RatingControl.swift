897ab9a9e578a8718b3e2a59bc4b71feb5ea1bac
Andy-E <student@macbook-pro-688-elder.local>
2016-10-31 09:29:39 -0400

    Got the new class setup.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,19 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Andy-E on 2016-10-31.
+//  Copyright © 2016 Andy-E. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+// MARK: Initialization
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}

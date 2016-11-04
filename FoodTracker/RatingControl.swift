c6a18bad5c36e788eac161ebc6e92343f956d16b
Andy-E <student@macbook-pro-688-elder.local>
2016-11-04 09:06:20 -0400

    Got the red box.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -14,6 +14,15 @@ class RatingControl: UIView {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        addSubview(button)
     }
 
+    override public var intrinsicContentSize: CGSize {
+        get {
+            return CGSize(width: 240, height: 44)
+        }
+    }
 }

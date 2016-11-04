ff8291200593f82f6bbe9e98241c21ce1ad6fb45
Andy-E <student@macbook-pro-688-elder.local>
2016-11-04 09:15:48 -0400

    Added stacked buttons.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class RatingControl: UIView {
+    
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
 
 // MARK: Initialization
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
+       for _ in 0..<5 {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchDown)
-        
+        ratingButtons += [button]
         addSubview(button)
+        }
+        
     }
 
     override public var intrinsicContentSize: CGSize {

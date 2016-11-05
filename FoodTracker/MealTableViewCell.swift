918a52a3f00349869333aa9d01d1f271a4e0e7bc
Andrew <aelder@rsgc.on.ca>
2016-11-05 11:04:06 -0400

    Got the storyboard for the table view somewhat setup

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Andy-E on 2016-11-05.
+//  Copyright © 2016 Andy-E. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
